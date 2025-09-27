fcc4e39728879913e16003c1f35c71d3d1db768a
it_cwc <1766987395@qq.com>
2025-09-28 06:56:14 +0800

    fix(utils): 修复导出功能中的代码格式问题
    调整 export.ts 文件中的代码缩进和常量定义，确保代码风格一致性和可读性。
    更新 markdown 转 HTML 逻辑中使用的主题 CSS URL，以匹配新的资源路径。

--- utils/export.ts
+++ utils/export.ts
@@ -122,8 +122,8 @@ export const exportDocx = (content: string, fileName: string) => {
         data instanceof Blob
           ? data
           : new Blob([data], {
-              type: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-            });
+            type: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+          });
       saveAs(blobData, `${fileName}.docx`);
     })
     .catch((error) => {
@@ -152,8 +152,8 @@ const generateFullHtml = (htmlContent: string, themeCssUrl: string): string => {
 const markdownToHTML = (content: string, theme: "light" | "dark") => {
   const themeCssUrl =
     theme === "dark"
-      ? "https://cnotely.itcwc.com/notion-style-dark.css"
-      : "https://cnotely.itcwc.com/notion-style-light.css";
+      ? "https://cnote.itcwc.com/notion-style-dark.css"
+      : "https://cnote.itcwc.com/notion-style-light.css";
 
   // 获取 mdtohtml 元素并清空其内容
   const mdtohtmlElement = document.getElementById("mdtohtml");
